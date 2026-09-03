e85f18c8fba7904e3fd41ad89e34bee45b99e848
AlmogStigg <almog@stigg.io>
2026-09-03 11:47:49 +0300

    release: 0.1.0-beta.48

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 
 allprojects {
     group = "io.stigg"
-    version = "0.1.0-beta.47" // x-release-please-version
+    version = "0.1.0-beta.48" // x-release-please-version
 }
 
 subprojects {
